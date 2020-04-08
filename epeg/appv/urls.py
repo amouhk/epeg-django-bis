6031fcf528ac6b5a604ace6d248390d42a4edb6b
AMOUH Kévin <kevin.amouh@atos.net>
2020-04-08 08:51:16 +0200

    page home activity about-us are done

--- epeg/appv/urls.py
+++ epeg/appv/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('accueil', views.home),
+    path('accueil',     views.home),
+    path('about',       views.about),
+    path('activity',    views.activity),
 ]
--- epeg/appv/views.py
+++ epeg/appv/views.py
@@ -4,3 +4,14 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'appv/index.html', locals())
+
+
+def about(request):
+    welcome = "Bienvenue à l 'Eglise Protestante Evangélique \n aux Gobelins"
+    return render(request, 'appv/about-us.html', locals())
+
+
+def activity(request):
+    welcome = "Bienvenue à l 'Eglise Protestante Evangélique \n aux Gobelins"
+    announce = "appv/pdf/programme_avril_2020.pdf"
+    return render(request, 'appv/activity.html', locals())
