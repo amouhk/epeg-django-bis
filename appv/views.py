b9632e244f1a97307471992affa1264173928e49
Kevin AMOUH <kevin.amouh@gmail.com>
2020-12-28 01:40:05 +0000

    ajout d'une carousel d'image sur la page d'accueil
    hide home page model

--- appv/views.py
+++ appv/views.py
@@ -16,18 +16,20 @@ from django import http
 
 
 
- 
+
 
 def home(request):
-    MEDIA_URL =  conf_settings.MEDIA_URL # Get audio url 
+    MEDIA_URL =  conf_settings.MEDIA_URL # Get audio url
 
     sermon_latest = Predication.objects.order_by('date').reverse()[0]
+    photo_carousel_active = Gallery.objects.filter(album="noel2020")[0]
+    photos_caroussel = Gallery.objects.filter(album="noel2020").reverse()
     return render(request, 'appv/index.html', locals())
 
 
 def about(request):
-    welcome = conf_settings.WELCOME_MSG
-    MEDIA_URL =  conf_settings.MEDIA_URL  
+    welcom_e = conf_settings.WELCOME_MSG
+    MEDIA_URL =  conf_settings.MEDIA_URL
 
     # Mail sending request
     if request.method == 'POST':
@@ -35,17 +37,17 @@ def about(request):
         form = ContactForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
-           # Send confirm email to visitor 
+           # Send confirm email to visitor
             send_mail(
                 subject=str("Confirmation de votre message à EPE des Goblelins"),
-                message="Bonjour " + str(form.cleaned_data["name"]) + "\n,"  + 
+                message="Bonjour " + str(form.cleaned_data["name"]) + "\n,"  +
                         "Vous avez ecris : " + "\n\n" +
 
                         str(form.cleaned_data['message']) + "\n\n"  +
 
                         str(""" Nous accusons de receptions votre pise de contact et vous repondrons rapidement.
-                        
-                        Fraternelement, 
+
+                        Fraternelement,
                         L'église des Gobelins."""),
 
                 from_email= conf_settings.EMAIL_HOST_USER,
@@ -53,19 +55,19 @@ def about(request):
                 fail_silently=False
             )
 
-           # Send visitor email to church official email 
+           # Send visitor email to church official email
             send_mail(
                 subject = str("Prise de contact depuis votre site "),
-                message="\n" + 
-                        "Sujet : " + str(form.cleaned_data['subject'])  + "\n" + 
-                        "Nom :" + str(form.cleaned_data['name']) + "\n" + 
-                        "Email: " + str(form.cleaned_data['email'])  + "\n" + 
+                message="\n" +
+                        "Sujet : " + str(form.cleaned_data['subject'])  + "\n" +
+                        "Nom :" + str(form.cleaned_data['name']) + "\n" +
+                        "Email: " + str(form.cleaned_data['email'])  + "\n" +
                         "Message: " + str(form.cleaned_data['message'])  + "\n",
                 from_email = conf_settings.EMAIL_HOST_USER,
                 recipient_list =[conf_settings.EMAIL_HOST_USER] ,
                 fail_silently=False
             )
-        
+
         form.clean()
         form = ContactForm()
         return http.HttpResponseRedirect('/appv/about#contact')
@@ -78,7 +80,7 @@ def about(request):
 @xframe_options_exempt
 def activity(request):
     welcome = conf_settings.WELCOME_MSG
-    MEDIA_URL = conf_settings.MEDIA_URL   
+    MEDIA_URL = conf_settings.MEDIA_URL
 
     agenda_current = Agenda.objects.order_by('date').last()
     agenda_history = Agenda.objects.order_by('date').reverse()
@@ -99,7 +101,7 @@ def gallery(request):
 def sermon(request):
     welcome = conf_settings.WELCOME_MSG
     MEDIA_URL =  conf_settings.MEDIA_URL
-    
+
     sermons = Predication.objects.order_by('date').reverse()
     device_mobile = 0
     user_agent = get_user_agent(request)
